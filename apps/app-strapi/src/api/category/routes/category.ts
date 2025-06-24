2725b45896b561e941ec4bb0c2e8379e5b05109b
KyiThantSin <kyits485@gmail.com>
2025-06-24 14:24:32 +0700

    feat: add category controller, router, and service for Strapi API

---
+++ apps/app-strapi/src/api/category/routes/category.ts
@@ -0,0 +1,7 @@
+/**
+ * cat router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::category.category');
---
+++ apps/app-strapi/src/api/category/services/category.ts
@@ -0,0 +1,7 @@
+/**
+ * cat service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::category.category');
