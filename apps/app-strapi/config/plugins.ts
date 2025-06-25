7330fceddaf34d3766c4a05f7efa75f268a6d62d
Ken Ling <ken88ling@gmail.com>
2025-06-25 14:53:41 +0800

    feat: integrate nodemailer email and AWS S3 upload providers for Strapi

--- apps/app-strapi/config/plugins.ts
+++ apps/app-strapi/config/plugins.ts
@@ -1,16 +1,58 @@
-export default ({ env: _env }) => ({
-  // Temporarily disable DO upload provider for testing
-  // upload: {
-  //   config: {
-  //     provider: 'strapi-provider-upload-do',
-  //     providerOptions: {
-  //       key: env('DO_SPACES_ACCESS_KEY_ID'),
-  //       secret: env('DO_SPACES_SECRET_ACCESS_KEY'),
-  //       endpoint: env('DO_SPACES_ENDPOINT'),
-  //       space: env('DO_SPACES_BUCKET'),
-  //       directory: 'media', // optional - will organize uploads in a media folder
-  //       cdn: env('DO_SPACES_CDN'), // optional - if you have a CDN configured
-  //     },
-  //   },
-  // },
-});
+module.exports = ({ env }) => {
+  return {
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.gmail.com'),
+          port: env('SMTP_PORT', 465),
+          secure: true,
+          auth: {
+            user: env('MAIL_USERNAME'),
+            pass: env('MAIL_PASSWORD'),
+          },
+          ignoreTLS: true,
+        },
+        settings: {
+          defaultFrom: env('SEND_FROM', 'dev.nexstack@gmail.com'),
+          defaultReplyTo: env('SEND_TO', 'dev.nexstack@gmail.com'),
+        },
+      },
+    },
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: true,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            credentials: {
+              accessKeyId: env('DO_SPACE_ACCESS_KEY'),
+              secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+            },
+            endpoint: env('DO_SPACE_ENDPOINT'),
+            region: env('DO_SPACE_REGION'),
+            forcePathStyle: true, // Required for DigitalOcean Spaces
+            params: {
+              Bucket: env('DO_SPACE_BUCKET'),
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
